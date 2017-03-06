e3cc640ac12dcfada9dab65c4c9617bab69c0baa
shi0303ma <matsukuma@matsukuma-no-macbook-pro.local>
2017-03-06 14:08:43 +0900

    FlappyBirdクローンの作成。SpriteKitを使って画像のアニメーション、背景スクロール自動化、衝突判定、タップ操作を可能にした。

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -1,25 +1,44 @@
-//
 //  ViewController.swift
 //  FlappyBird
 //
 //  Created by 松隈璃奈 on 2017/03/02.
 //  Copyright © 2017年 shi0303ma. All rights reserved.
-//
+
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // SKViewに型を変換する
+        let skView = self.view as! SKView
+        
+        // FPSを表示する
+        skView.showsFPS = true
+        
+        // ノードの数を表示する
+        skView.showsNodeCount = true
+        
+        // ビューと同じサイズでシーンを作成する
+        let scene = GameScene(size:skView.frame.size)
+        
+        // ビューにシーンを表示する
+        skView.presentScene(scene)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // ステータスバーを消す
+    override var prefersStatusBarHidden: Bool {
+        get {
+            return true
+        }
+    }
 }
 
